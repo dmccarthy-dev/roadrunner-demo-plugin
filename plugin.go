fb3ca5cfa7259475e8cc74e66ab693ba477af37e
dmccarthy <dmccarthy@bluefin.com>
2024-09-11 10:56:13 +0100

    First commit

---
+++ plugin.go
@@ -0,0 +1,69 @@
+package roadrunner_demo_plugin
+
+import (
+	"context"
+	"github.com/roadrunner-server/errors"
+	"go.uber.org/zap"
+)
+
+const PluginName = "roadrunner_demo_plugin"
+
+type Plugin struct {
+	cfg    *Config
+	logger *zap.Logger
+}
+
+type Configurer interface {
+	// UnmarshalKey takes a single key and unmarshal it into a Struct.
+	UnmarshalKey(name string, out any) error
+	// Has checks if config section exists.
+	Has(name string) bool
+}
+
+type Logger interface { // <-- logger plugin implements
+	NamedLogger(name string) *zap.Logger
+}
+
+func (p *Plugin) Init(cfg Configurer, log Logger) error {
+	p.logger = log.NamedLogger("roadrunner_demo_plugin")
+	p.logger.Info("start initializing demo plugin")
+	const op = errors.Op("custom_plugin_init")
+
+	if !cfg.Has(PluginName) {
+		return errors.E(op, errors.Disabled)
+	}
+
+	err := cfg.UnmarshalKey(PluginName, &p.cfg)
+	if err != nil {
+		// Error will stop execution
+		return errors.E(op, err)
+	}
+
+	p.cfg.InitDefaults()
+
+	p.logger.Info("end initializing demo plugin")
+	return nil
+}
+
+func (p *Plugin) Serve() chan error {
+	const op = errors.Op(PluginName)
+	errCh := make(chan error, 1)
+
+	err := p.DoSomeWork()
+	if err != nil {
+		errCh <- errors.E(op, err)
+		return errCh
+	}
+
+	return nil
+}
+
+func (p *Plugin) Stop(_ context.Context) error {
+	p.logger.Info("Stopping plugin")
+	return nil
+}
+
+func (p *Plugin) DoSomeWork() error {
+	p.logger.Info("DoSomeWork")
+	return nil
+}
